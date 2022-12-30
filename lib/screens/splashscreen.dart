421ca12c4fd2662c1370df064b8d22fd0a455de9
altrono <hbotema16@gmail.com>
2022-12-30 23:33:52 +0200

    Added HomeScreen

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,33 @@
+import 'package:cat_dog_detector/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplash extends StatefulWidget {
+  const MySplash({Key? key}) : super(key: key);
+
+  @override
+  State<MySplash> createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: HomeScreen(),
+      title: const Text(
+        'Dog a Cat',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+          color: Color(
+              0xFFFFB100,
+          )
+        ),
+      ),
+      image: Image.asset('assets/catdog.png'),
+      backgroundColor: Color(0xFFF0ECCF),
+      loaderColor: Color(0xFFA3BB98),
+    );
+  }
+}
